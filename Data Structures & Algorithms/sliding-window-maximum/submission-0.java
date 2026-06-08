207a4903ae2a3c57eb79887bd59619bb16d6ff26
Les <45894506+reysmt@users.noreply.github.com>
2026-06-08 23:09:27 +0200

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int l = 0;
+        ArrayList<Integer> results = new ArrayList<>();
+        for(int r = k - 1; r < nums.length; r++) {
+            l = r - (k - 1);
+            //window that starts from left to right
+
+            //while or for that check all nums inside the window
+            //store the max value inside an array
+            int maxValue = 0;
+            for(int i = l; i <= r; i++){
+                maxValue = Math.max(maxValue, nums[i]);
+            }
+            results.add(maxValue);
+        }
+        return results.stream()
+                      .mapToInt(Integer::intValue)
+                      .toArray();  
+    }
+}
