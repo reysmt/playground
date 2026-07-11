cbaf6331479a086b3e7992c814ff57f996d5d863
Les <45894506+reysmt@users.noreply.github.com>
2026-07-11 11:26:48 +0200

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    private boolean dfs(TreeNode node, int rootVal){
+        if(node == null){
+            return true;
+        }
+
+        if(node.left != null && node.left.val >= rootVal){
+            return false;
+        }
+
+        if(node.right != null && node.right.val <= rootVal){
+            return false;
+        }
+
+        dfs(node.left, node.val);
+        dfs(node.right, node.val);
+
+        return true;
+
+    }
+}
