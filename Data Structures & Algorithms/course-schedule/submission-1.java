032a26a6ef11c0d7101369164079edd18987cac0
Les <45894506+reysmt@users.noreply.github.com>
2026-08-15 16:22:32 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites.length == 0 || prerequisites[0].length == 0){
+            return true;
+        }
+        Set<Integer> seen = new HashSet<>();
+        int ROWS = prerequisites.length, COLS = prerequisites[0].length;
+
+        return detectCycle(numCourses, prerequisites, seen);
+    }
+
+    private boolean detectCycle(int numCourses, int[][] prerequisites, Set<Integer> seen){
+        int ROWS = prerequisites.length;
+        int COLS = prerequisites[0].length;
+
+        for(int r = 0; r < ROWS; r ++){
+            for (int c = 0; c < COLS; c++){
+                if(!seen.add(prerequisites[r][c])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
