d122786e7bbdc5769f5d91d8b6d2904c59234f9f
Les <45894506+reysmt@users.noreply.github.com>
2026-08-26 09:51:34 +0200

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+        int[] table = new int[n+1];
+        
+        table[1] = 1;
+        table[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            table[i] = table[i-1] + table[i-2];
+        }
+
+        return table[n];
+    }
+}
